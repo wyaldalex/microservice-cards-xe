2acbb33fa865393462985e7d0d2033bc858f7a48
Alexander Alvarenga <moltron_nano@hotmail.com>
2022-05-11 21:50:38 -0600

    feat: initial microservice setup

---
+++ src/main/java/com/tudux/cards/controller/CardsController.java
@@ -0,0 +1,36 @@
+package com.tudux.cards.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tudux.cards.model.Cards;
+import com.tudux.cards.model.Customer;
+import com.tudux.cards.repository.CardsRepository;
+
+/**
+ * @author Eazy Bytes
+ *
+ */
+
+@RestController
+public class CardsController {
+
+	@Autowired
+	private CardsRepository cardsRepository;
+
+	@PostMapping("/myCards")
+	public List<Cards> getCardDetails(@RequestBody Customer customer) {
+		List<Cards> cards = cardsRepository.findByCustomerId(customer.getCustomerId());
+		if (cards != null) {
+			return cards;
+		} else {
+			return null;
+		}
+
+	}
+
+}
---
+++ src/main/java/com/tudux/cards/repository/CardsRepository.java
@@ -0,0 +1,16 @@
+package com.tudux.cards.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tudux.cards.model.Cards;
+
+@Repository
+public interface CardsRepository extends CrudRepository<Cards, Long> {
+
+	
+	List<Cards> findByCustomerId(int customerId);
+
+}
