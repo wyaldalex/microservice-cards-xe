ef84144a11d06317a5c1f07797a15a988d7288fc
Alexander Alvarenga <moltron_nano@hotmail.com>
2022-05-22 15:06:01 -0600

    feat: add config server testing

--- src/main/java/com/tudux/cards/controller/CardsController.java
+++ src/main/java/com/tudux/cards/controller/CardsController.java
@@ -3,12 +3,18 @@ package com.tudux.cards.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.tudux.cards.config.CardsServiceConfig;
 import com.tudux.cards.model.Cards;
 import com.tudux.cards.model.Customer;
+import com.tudux.cards.model.Properties;
 import com.tudux.cards.repository.CardsRepository;
 
 /**
@@ -21,6 +27,9 @@ public class CardsController {
 
 	@Autowired
 	private CardsRepository cardsRepository;
+	
+	@Autowired
+	CardsServiceConfig cardsConfig;
 
 	@PostMapping("/myCards")
 	public List<Cards> getCardDetails(@RequestBody Customer customer) {
@@ -31,6 +40,15 @@ public class CardsController {
 			return null;
 		}
 
+	}
+	
+	@GetMapping("/cards/properties")
+	public String getPropertyDetails() throws JsonProcessingException {
+		ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+		Properties properties = new Properties(cardsConfig.getMsg(), cardsConfig.getBuildVersion(),
+				cardsConfig.getMailDetails(), cardsConfig.getActiveBranches());
+		String jsonStr = ow.writeValueAsString(properties);
+		return jsonStr;
 	}
 
 }
