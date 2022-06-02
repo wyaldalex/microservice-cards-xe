d79e210b379d074befb8eebfb502e2d6c50be6a1
Alexander Alvarenga <moltron_nano@hotmail.com>
2022-06-01 21:55:28 -0600

    feat: add micrometer support

--- src/main/java/com/tudux/cards/CardsApplication.java
+++ src/main/java/com/tudux/cards/CardsApplication.java
@@ -3,6 +3,10 @@ package com.tudux.cards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.aop.TimedAspect;
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @RefreshScope
@@ -11,5 +15,10 @@ public class CardsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CardsApplication.class, args);
 	}
+	
+	@Bean
+	public TimedAspect timedAspect(MeterRegistry registry) {
+	    return new TimedAspect(registry);
+	}	
 
 }
